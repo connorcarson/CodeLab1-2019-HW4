264771685fe379dd3b7b30ad5405e79685b3e1b5
connorcarson <cc3237@nyu.edu>
2019-03-04 01:33:06 -0500

    Made canvas into singleton using UIController script and eliminated the LevelManager entirely. All display methods for UI are now entrusted to the GameManager as are Level Loading and Game Over states. Time and health is successfully reset in the canvas while score is carried over upon loading a new level. Removed obsolete GameOverManager script. Bug: dropdown menu now retains the level selected but AFTER player quits.

--- CodeLab1Week4/Assets/Scripts/GameManager.cs
+++ CodeLab1Week4/Assets/Scripts/GameManager.cs
@@ -7,12 +7,34 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	public float initCubeBlueSpawnDelay = 3;
 	public float initCubeRedSpawnDelay = 5.5f;
 	public float cubeSpawnRate = 8;
+	
+	public float timeLeft = 30;
+	public float timeMax = 30;
+	private int wholeTime;
+	
+	public int sceneIndex;
+	
+	public float restartTimer;
+	public float restartDelay = 8;
+	
+	public TextMeshProUGUI timerText;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI healthText;
+	public TextMeshProUGUI highScoreText;
+	
+	private Animator anim;
+	
+	public Canvas canvas;
+	
+	private List<string> difficultyLevel = new List<string>() {"Easy", "Normal", "Difficult"};
+	public Dropdown dropdown;
 
 	private const string MyHighScore_File = "/MyHighScore.txt";
 	
@@ -28,10 +50,13 @@ public class GameManager : MonoBehaviour
 				health = 100;
 			}
 
-			if (health < 0)
+			if (health <= 0)
 			{
 				health = 0;
+				GameOver();
 			}
+
+			healthText.text = "Health: " + health;
 		}
 	}
 
@@ -45,6 +70,7 @@ public class GameManager : MonoBehaviour
 		set
 		{
 			score = value;
+			scoreText.text = "Score: " + score;
 			HighScore = score;
 		}
 	}
@@ -61,8 +87,28 @@ public class GameManager : MonoBehaviour
 			if (value > highScore)
 			{
 				highScore = value;
+				highScoreText.text = "High Score: " + highScore;
 				string fullPathToFile = Application.dataPath + MyHighScore_File; //create string for path to MyHighScore file
 				File.WriteAllText(fullPathToFile, "The current high score is " + highScore); //write HighScore in file
+			}
+		}
+	}
+	
+	public float TimeLeft
+	{
+		get { return timeLeft; }
+		set
+		{
+			timeLeft = value;
+			if (timeLeft > timeMax)
+			{
+				timeLeft = timeMax;
+			}
+
+			if (timeLeft <= 0)
+			{
+				timeLeft = 0;
+				LevelLoader();
 			}
 		}
 	}
@@ -82,6 +128,8 @@ public class GameManager : MonoBehaviour
 			Destroy(gameObject); //otherwise, if there is an instance already in this scene, destroy this LevelManager
 		}
 
+		anim = canvas.GetComponent<Animator>();
+		
 		string myHighScoreFileText = File.ReadAllText(Application.dataPath + MyHighScore_File); //get text from myHighScore file
 		string[] highScoreSplit = myHighScoreFileText.Split(' '); //divide text by spaces
 		HighScore = Int32.Parse(highScoreSplit[5]); //the high score number in file to int and set HighScore to that int
@@ -89,12 +137,32 @@ public class GameManager : MonoBehaviour
 		HeartSpawn(); //spawn first prize at the start of our game
 		InvokeRepeating("CubeBlueSpawn", initCubeBlueSpawnDelay, cubeSpawnRate); //spawn CubeBlue according to our init delay in seconds, and then repeat according to our cubeSpawnRate
 		InvokeRepeating("CubeRedSpawn", initCubeRedSpawnDelay, cubeSpawnRate); //spawn CubeRed according to our init delay in seconds, and then repeat according to our cubeSpawnRate
+		
+		dropdown.AddOptions(difficultyLevel);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		LevelTimer();
 		CheckForPrize();
+		
+		/*if (timeLeft <= 0)
+		{
+			LevelLoader();
+		}
+
+		if (Health <= 0)
+		{
+			GameOver();
+		}*/
+	}
+	
+	void LevelTimer()
+	{
+		TimeLeft -= Time.deltaTime; //Countdown one second, every second
+		wholeTime = (int) TimeLeft; //Convert float to int, round time in seconds up to whole number
+		timerText.text = "" + wholeTime; //display Time
 	}
 
 	void HeartSpawn() //function for spawning our heart prize
@@ -123,4 +191,60 @@ public class GameManager : MonoBehaviour
 			HeartSpawn(); //then spawn a new prize
 		}
 	}
+	
+	void LevelLoader()
+	{
+		anim.SetTrigger("Level1Over");
+		restartTimer += Time.deltaTime; //count up in seconds
+		if (restartTimer >= restartDelay) //if restart timer is equal to our restart delay
+		{
+			timeLeft = timeMax;
+			SceneManager.LoadScene(sceneIndex); //load next level
+		}
+	}
+    
+	public void GameOver()
+	{
+		anim.SetTrigger("GameOver"); //start Game Over animation
+        
+		restartTimer += Time.deltaTime; //count up in seconds
+		if (restartTimer >= restartDelay) //if restart timer is equal to our restart delay
+		{
+			Health = 100;
+			Score = 0;
+			timeLeft = timeMax;
+			SceneManager.LoadScene(0); //load first level
+		}
+	}
+	
+	public void DifficultySetting(int difficultyIndex)
+    {
+        if (difficultyIndex == 0)
+        {
+	        SceneManager.LoadScene(0);
+	        timeLeft = 90;
+            timeMax = 90;
+            instance.initCubeRedSpawnDelay = 5;
+            instance.initCubeBlueSpawnDelay = 8;
+            instance.cubeSpawnRate = 10;
+        }
+        else if (difficultyIndex == 1)
+        {
+	        SceneManager.LoadScene(0);
+	        timeLeft = 60;
+            timeMax = 60;
+            initCubeRedSpawnDelay = 3.5f;
+            initCubeBlueSpawnDelay = 6f;
+            cubeSpawnRate = 8;
+        } 
+        else if (difficultyIndex == 2)
+        {
+	        SceneManager.LoadScene(0);
+	        timeLeft = 30;
+            timeMax = 30;
+            initCubeRedSpawnDelay = 1.5f;
+            initCubeBlueSpawnDelay = 3;
+            cubeSpawnRate = 5;
+        }
+    }
 }
