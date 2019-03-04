ca0ff7bd70ff69f906c4f49724e016a4220c009b
connorcarson <cc3237@nyu.edu>
2019-03-03 20:06:40 -0500

    Init commit of unity project (copy of previous week's project)

---
+++ CodeLab1Week4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public float initCubeSpawnDelay = 3;
+	public float cubeSpawnRate = 8;
+	
+
+	int health = 100;
+
+	public int Health
+	{
+		get { return health; }
+		set
+		{
+			health = value;
+			if (health > 100)
+			{
+				health = 100;
+			}
+
+			if (health < 0)
+			{
+				health = 0;
+			}
+		}
+	}
+
+	int score = 0;
+
+	public int Score
+	{
+		get
+		{
+			return score;
+		}
+		set
+		{
+			score = value;
+		}
+	}
+	
+	public static GameManager instance;
+	
+	// Use this for initialization
+	void Start()
+	{	
+		if (instance == null) //if there is no other instance of LevelManager already in the scene
+		{
+			DontDestroyOnLoad(gameObject); //don't destroy this LevelManager
+			instance = this; //set the current instance to this LevelManager
+		}
+		else
+		{
+			Destroy(gameObject); //otherwise, if there is an instance already in this scene, destroy this LevelManager
+		}
+		
+		HeartSpawn(); //spawn first prize at the start of our game
+		InvokeRepeating("CubeSpawn", initCubeSpawnDelay, cubeSpawnRate); //spawn CubeRed and CubeBlue according to our init delay in seconds, and then repeat according to our cubeSpawnRate
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		CheckForPrize();
+	}
+
+	void HeartSpawn() //function for spawning our heart prize
+	{
+		GameObject newPrize = Instantiate(Resources.Load<GameObject>("Prefabs/Prize")); //loads prefab into game
+		newPrize.transform.position = new Vector3(Random.Range(-10, 10), Random.Range(-4, 4), 0.78f); //at new, random location
+	}
+
+	void CubeSpawn() //function for spawning our cube prizes
+	{
+		GameObject newCube1 = Instantiate(Resources.Load<GameObject>("Prefabs/CubeBlue"));
+		newCube1.transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-4, 4));
+		GameObject newCube2 = Instantiate(Resources.Load<GameObject>("Prefabs/CubeRed"));
+		newCube2.transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-4, 4));
+	}
+
+	void CheckForPrize() //check if heart prize has been destroyed
+	{
+		GameObject prizesInScene = GameObject.FindWithTag("Prize"); //find all objects in scene tagged "Prize"
+		if (prizesInScene == null) //if there are no objects tagged "Prize" in our scene
+		{
+			HeartSpawn(); //then spawn a new prize
+		}
+	}
+}
---
+++ CodeLab1Week4/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Canvas canvas;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+
+    private Animator anim;
+
+    public float timeLeft = 30;
+    private int wholeTime;
+    public float restartTimer;
+    public float restartDelay = 8;
+
+    public float TimeLeft
+    {
+        get { return timeLeft; }
+        set
+        {
+            timeLeft = value;
+            if (timeLeft > 30)
+            {
+                timeLeft = 30;
+            }
+
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+            }
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = canvas.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelTimer();
+        ScoreDisplay();
+        HealthDisplay();
+        
+        if (timeLeft <= 0)
+        {
+            LevelLoader();
+        }
+
+        if (GameManager.instance.Health <= 0)
+        {
+            GameOver();
+        }
+    }
+    
+    void LevelTimer()
+    {
+        TimeLeft -= Time.deltaTime; //Countdown one second, every second
+        wholeTime = (int) TimeLeft; //Convert float to int, round time in seconds up to whole number
+        timerText.text = "" + wholeTime; //display Time
+    }
+    
+    void ScoreDisplay()
+    {
+        scoreText.text = "Score: " + GameManager.instance.Score;
+    }
+
+    void HealthDisplay()
+    {
+        healthText.text = "Health: " + GameManager.instance.Health;
+    }
+
+    void LevelLoader()
+    {
+        anim.SetTrigger("Level1Over");
+        restartTimer += Time.deltaTime; //count up in seconds
+        if (restartTimer >= restartDelay) //if restart timer is equal to our restart delay
+        {
+            SceneManager.LoadScene(1); //load next level
+        }
+    }
+    
+    public void GameOver()
+    {
+        anim.SetTrigger("GameOver"); //start Game Over animation
+        
+        restartTimer += Time.deltaTime; //count up in seconds
+        if (restartTimer >= restartDelay) //if restart timer is equal to our restart delay
+        {
+            SceneManager.LoadScene(0); //load next level
+        }
+    }
+}
